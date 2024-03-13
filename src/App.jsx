016cb88b41333cc438379dac6f1753a9eee66aee
Roton02 <rph645102@gmail.com>
2024-03-13 17:01:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center bg-pink-400'>Knowledge with react</h1>
+    </>
+  )
+}
+
+export default App
